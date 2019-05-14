61f198df514ada9e1fc338b17d705c5d43c6996e
Marcos Henrique <wakeupmh@gmail.com>
2019-05-14 12:22:38 -0300

    Update variables.js

--- foodProject/api/bin/configuration/variables.js
+++ foodProject/api/bin/configuration/variables.js
@@ -3,7 +3,7 @@ const variables = {
         port: process.env.port || 3000
     },
     Database:{
-        connection: process.env.connection || "mongodb+srv://fitfoodadmin:senha@cluster0-x526p.mongodb.net/fitfood?retryWrites=true"
+        connection: process.env.connection || "mongodb+srv://<domain>:senha@cluster0-x526p.mongodb.net/<document>?retryWrites=true"
     },
     Security:{
         secretKey: '2d59fbb85d86f038d0c9ce012b601410'
