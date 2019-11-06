6cc3b15dbace35b957cf3db663a6b72b75a639e9
wakeupmh <wakeupmh@gmail.com>
2019-11-06 15:21:02 -0300

    this and bind

--- Review/this-and-bind.js
+++ Review/this-and-bind.js
@@ -1,7 +1,10 @@
 const people ={
     hello: 'Good Morning',
     speak () {
-        console.log(this.hello)
+        console.log(this.hello);
     }
-}
+};
 people.speak();
+
+const peopleSpeaking = pessoa.falar.bind(people); // this resolved for people
+peopleSpeaking();
