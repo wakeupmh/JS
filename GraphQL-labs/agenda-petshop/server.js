77e94056bacd3c0e1a79914cdafce33473a966b3
wakeupmh <wakeupmh@gmail.com>
2019-11-06 18:11:32 -0300

    graphql instead express

---
+++ GraphQL-labs/agenda-petshop/server.js
@@ -0,0 +1,26 @@
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/database/tabelas')
+const { GraphQLServer } = require('graphql-yoga')
+
+conexao.connect(erro => {
+  if (erro) {
+    console.log(erro)
+  }
+
+  console.log('conectou no banco')
+
+  Tabelas.init(conexao)
+})
+
+const resolvers = {
+  Query:{
+    status: () => "Servidor rodando!"
+  }
+}
+
+const servidor = new GraphQLServer({
+  resolvers,
+  typeDefs: './schema.graphql'
+})
+
+servidor.start(() => console.log('servidor ouvindo'));
