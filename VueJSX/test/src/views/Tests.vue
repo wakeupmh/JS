cd638607ee8987fc4574b78516ecf4aef76a43b8
wakeupmh <wakeupmh@gmail.com>
2019-12-06 13:36:32 -0300

    using jsx in vue and testing

---
+++ VueJSX/test/src/views/Tests.vue
@@ -0,0 +1,32 @@
+<script>
+    export default {
+        methods: {
+            greater(){
+                let greaterClass = {
+                    find: nums =>{
+                        const min = Number.MAX_VALUE;
+                        const max = Number.MIN_VALUE;
+                        nums.forEach(element => {
+                            if(num < min) 
+                                min = num;
+                            else if(num > max)
+                                max = num;
+                        });
+                    },
+                    getMin: () => min,
+                    getMax: () => max
+                }
+                return greaterClass
+            }
+        },
+        render () { 
+            return (
+            <div>
+                <div class="about">
+                <h1>This is an about page</h1>
+                </div>
+            </div>
+            ) 
+        }
+    }
+</script>
