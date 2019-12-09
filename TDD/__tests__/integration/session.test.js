e009fbc77002e77622befc193f47790daf14279a
wakeupmh <wakeupmh@gmail.com>
2019-12-09 14:51:21 -0300

    implementing jest

---
+++ TDD/__tests__/integration/session.test.js
@@ -0,0 +1,13 @@
+describe('Authentication', () =>{
+  // it('should receive JWT token when authenticated with valid credentials', () => {
+    
+  // });
+  it('should sum two numbers', () =>{
+    const x = 3;
+    const y = 4;
+
+    const sum = x + y;
+
+    expect(sum).toBe(7);
+  })
+})
