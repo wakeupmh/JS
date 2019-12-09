c8a8e41636f86512fe1f69110e4e994c5805d1b9
wakeupmh <wakeupmh@gmail.com>
2019-12-09 14:34:26 -0300

    adding jest

--- TDD/src/routes.js
+++ TDD/src/routes.js
@@ -1,9 +1,3 @@
 const routes = require('express').Router();
-const { user } = require('./app/models');
-user.create({ 
-    name: "Dylan",
-    email: "dylan@gmail.com",
-    password_hash: "sd3228djs"
-});
 
 module.exports = routes;
