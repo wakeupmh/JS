7a86ebcb815c56506c282cf278bef710d97d10c4
wakeupmh <wakeupmh@gmail.com>
2019-12-26 13:26:28 -0300

    starting a rest api for suggestion words with similarity

---
+++ NLP/suggest-words/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express();
+
+let routes = require('./routes/routes');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '10mb'}));
+
+routes.assignRoutes(app);
+
+app.listen(3000);
