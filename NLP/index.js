c931ba3de1f3996c9ec9803e900a54856024a021
wakeupmh <wakeupmh@gmail.com>
2019-12-26 12:55:59 -0300

    Naive Bayes classifier

--- NLP/index.js
+++ NLP/index.js
@@ -10,3 +10,13 @@ console.log("I can see that we are going to be friends".tokenizeAndStem());
 console.log(natural.HammingDistance("karolin", "kathrin", false));
 console.log(natural.HammingDistance("inae", "iena", false));
 console.log(natural.HammingDistance("short string", "longer string", false));
+
+let classifier = new natural.BayesClassifier();
+classifier.addDocument('i am long qqqq', 'buy');
+classifier.addDocument('buy the q\'s', 'buy');
+classifier.addDocument('short gold', 'sell');
+classifier.addDocument('sell gold', 'sell');
+classifier.train();
+
+console.log(classifier.classify('i am short silver'));
+console.log(classifier.classify('i am long copper'));
