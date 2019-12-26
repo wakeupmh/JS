74cc598b3929d4d1ef45576934d391306eec5f9c
wakeupmh <wakeupmh@gmail.com>
2019-12-26 12:51:28 -0300

    Porter Stemming Algorithm

--- NLP/index.js
+++ NLP/index.js
@@ -3,3 +3,6 @@ const natural = require('natural');
 let tokenizer = new natural.WordTokenizer();
 
 console.log(tokenizer.tokenize("The quick brown fox jumps over the lazy dog"));
+
+natural.PorterStemmer.attach();
+console.log("I can see that we are going to be friends".tokenizeAndStem());
