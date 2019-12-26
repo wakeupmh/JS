e4b677e0d0704f79590e71b333e2ac15d8cfbf74
wakeupmh <wakeupmh@gmail.com>
2019-12-26 12:53:35 -0300

    Measuring the similarity between words with Hamming Distance Algorithm

--- NLP/index.js
+++ NLP/index.js
@@ -6,3 +6,7 @@ console.log(tokenizer.tokenize("The quick brown fox jumps over the lazy dog"));
 
 natural.PorterStemmer.attach();
 console.log("I can see that we are going to be friends".tokenizeAndStem());
+
+console.log(natural.HammingDistance("karolin", "kathrin", false));
+console.log(natural.HammingDistance("inae", "iena", false));
+console.log(natural.HammingDistance("short string", "longer string", false));
