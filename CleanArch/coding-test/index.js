342b8eced7516dedc60f5495d6c510cf369b9ce2
wakeupmh <wakeupmh@gmail.com>
2020-01-03 00:00:52 -0300

    mapping  stdin

--- CleanArch/coding-test/index.js
+++ CleanArch/coding-test/index.js
@@ -1,7 +1,6 @@
 const testWeekDay = require('./use_case/isWeekend')
 process.stdin.on('readable', () => {
   const userInput = process.stdin.read().toString()
-  console.log(userInput)
   const splited = userInput.split(',')
   splited.map(x => {
     process.stdout.write(`Your Input is weekend: ${testWeekDay.isWeekend(x)}\n`)
