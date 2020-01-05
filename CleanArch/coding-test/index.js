197769160a7b142e00f39e759727f191237d8f42
wakeupmh <wakeupmh@gmail.com>
2020-01-05 10:38:29 -0300

    adding use_case handler

--- CleanArch/coding-test/index.js
+++ CleanArch/coding-test/index.js
@@ -1,10 +1,9 @@
-const weekDay = require('./use_case/isWeekend')
-const reward = require('./use_case/isReward')
+// const cheaperHotel = require('./use_case/cheaperHotel')
 
 process.stdin.on('readable', () => {
   const userInput = process.stdin.read().toString()
   const splited = userInput.split(',')
   splited.map(x => {
-    process.stdout.write(`Your Input is weekend: ${weekDay.isWeekend(x)}\nIs Reward: ${reward.isReward(x)}\n`)
+    // process.stdout.write(`Your Input is weekend: ${weekDay.isWeekend(x)}\nIs Reward: ${reward.isReward(x)}\n`)
   })
 })
---
+++ CleanArch/coding-test/use_case/cheaperHotel.js
@@ -0,0 +1,6 @@
+// const weekDay = require('./use_case/isWeekend')
+// const reward = require('./use_case/isReward')
+
+exports.findCheaper = string => {
+
+}
