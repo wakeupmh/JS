317db9ec57098011d11a40274d1bf0da5ac8c188
wakeupmh <wakeupmh@gmail.com>
2020-01-29 01:51:45 -0300

    adding vue

---
+++ Review/color-game/vue/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <ColorContainer>
+      <ColorSquare 
+        v-for="colors in color"
+        :key="color"
+      />
+    </ColorContainer>
+  </div>
+</template>
+
+<script>
+import ColorContainer from "@/components/ColorContainer";
+import ColorSquare from "@/components/ColorSquare";
+
+export default {
+  name: "app",
+  data: () => ({
+
+  }),
+  components: {
+    ColorContainer, ColorSquare
+  },
+  methods: {
+    getColor (colorsArray) {
+      let randomColor = Math.floor(Math.random() * colorsArray.length);
+      return colorsArray[randomColor];
+    },
+    createRandomColors (quantity) {
+      let colorsArray = [];
+      for (let i = 0; i < quantity; i++) {
+        colorsArray.push(this.randomizeColor());
+      }
+      return colorsArray;
+    },
+    randomizeColor () {
+      let r = this.mathRandom();
+      let g = this.mathRandom();
+      let b = this.mathRandom();
+      return `rgb(${r}, ${g}, ${b})`;
+    },
+    mathRandom () {
+      return Math.floor(Math.random() * 256);
+    }
+  },
+};
+</script>
+
---
+++ Review/color-game/vue/src/components/ColorSquare.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="colorSquare"
+  :style="`background-color:${color}`" />
+</template>
+
+<script>
+ export default {
+  props:{
+    color: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
