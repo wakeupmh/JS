59c854bd9a39ce5d52b0d45bbdff567328073fa0
wakeupmh <wakeupmh@gmail.com>
2020-02-25 00:34:32 -0300

    config typescript

--- Review/bank-app/app/ts/models/Negociacao.ts
+++ Review/bank-app/app/ts/models/Negociacao.ts
@@ -1,34 +1,28 @@
 class Negociacao {
+  private _data;
+  private _quantidade;
+  private _valor;
+  
+  constructor(data, quantidade, valor) {
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
+  }
 
-    private _data;
-    private _quantidade;
-    private _valor;
-    
-    constructor(data, quantidade, valor) {
+  get data() {
+    return this._data;
+  }
 
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+  get quantidade() {
+    return this._quantidade;
+  }
 
-    get data() {
+  get valor() {
+    return this._valor;
+  }
 
-        return this._data;
-    }
-
-    get quantidade() {
-
-        return this._quantidade;
-    }
-
-    get valor() {
-
-        return this._valor;
-    }
-
-    get volume() {
-
-        return this._quantidade * this._valor;
-    }
+  get volume() {
+    return this._quantidade * this._valor;
+  }
 }
 
